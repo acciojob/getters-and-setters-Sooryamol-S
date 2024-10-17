4f0ae33873bfc642d716301ef1ecccd4f9a57980
Sooryamol-S <135025674+Sooryamol-S@users.noreply.github.com>
2024-10-17 13:35:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-//complete this code
-class Person {}
+class Person {
+    // Constructor method with name and age
+    constructor(name, age) {
+        this._name = name; // Using underscore to denote private variables
+        this._age = age;
+    }
 
-class Student extends Person {}
+    // Getter for name
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    // Setter for age
+    set age(age) {
+        this._age = age;
+    }
+
+    // Getter for age (optional)
+    get age() {
+        return this._age;
+    }
+}
+
+// Student class extends Person
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+// Teacher class extends Person
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
